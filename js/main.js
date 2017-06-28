3550ccebd7609840f52626c76111343477edeb13
Bobby Erickson <roericks@cisco.com>
2017-06-28 11:25:40 -0400

    Cards now flipping properly and flip back over after 2 clicks

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ function shuffleArray(arr){
     }
 }
 
+//creates a new, blank card board.  Is set off on page load and when users want to play a new game.
 function createBoard(){
 
     // cloning 5 cards into a row
@@ -42,11 +43,11 @@ function createBoard(){
 
     for(var i = 0; i < card_icons.length; i++){
         //creating an image tag to place in our card div
-        var image = '<img src="sports-icons/svg/' + card_icons[i] + '.svg" />';
+        var image = '<img src="sports-icons/svg/' + card_icons[i] + '.svg" data-image="' + card_icons[i] + '"/>';
 
         // Going through each of our cards.  When we come across the first empty card,
         // we will put that icon inside that card.
-        $('.card').each(function(){
+        $('.back').each(function(){
             if($(this).is(':empty')){
                 $(this).prepend(image);
                 return false
@@ -55,6 +56,40 @@ function createBoard(){
     }
 }
 
+// will be used in flipCard.  Keeping track of how many times user has flipped cards.
+var click_count = 0;
 
+function flipCard(){
+    var card = $(this);
+
+    //preventing users from flipping the same card
+    if(card.hasClass('flipped')){
+        return;
+    }
+    //once user has clicked card, we increase click_count and flip card
+    else {
+        click_count++;
+        card.addClass('flipped');
+
+        //once user has clicked card twice, we use setTimeout() to flip cards back over after 2 seconds.
+        if(click_count >= 2){
+            // This is unbinding our flipCard click event from all effect_click classes.
+            // It will stop users from being able to flip other cards during setTimeout.
+            $('.effect_click').off('click', flipCard);
+
+            setTimeout(function(){
+                $('.flipped').removeClass('flipped');
+                click_count = 0;
+                // re-binding our flipCard click event to our cards.
+                $('.effect_click').on('click', flipCard);
+            }, 3000);
+        }
+    }
+}
 
 createBoard();
+
+$('.effect_click').on('click', flipCard);
+
+
+
